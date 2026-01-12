7e392733efc8458292b104a15d30e92fb9e2910a
Cutiee <falturoaster18@gmail.com>
2026-01-12 14:58:30 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIpdtOgSsv_PKJnA0kMk7fhqqD4yNaeZI",
+  authDomain: "sayitcutie.firebaseapp.com",
+  projectId: "sayitcutie",
+  storageBucket: "sayitcutie.firebasestorage.app",
+  messagingSenderId: "559001612992",
+  appId: "1:559001612992:web:593f8c9f41d1e9bfbe7325"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.sendMsg = async function () {
+  const msg = document.getElementById("msg").value.trim();
+  if (!msg) return;
+
+  await addDoc(collection(db, "messages"), {
+    text: msg,
+    createdAt: serverTimestamp()
+  });
+
+  document.getElementById("msg").value = "";
+  document.getElementById("status").innerText = "Sent anonymously 💗";
+};
