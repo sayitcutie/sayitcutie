2498ac06ef4134180d6004667e31cd8059482ff2
Cutiee <falturoaster18@gmail.com>
2026-01-29 01:59:35 +0530

    Update u.js

--- u/u.js
+++ u/u.js
@@ -1,7 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-
+const firebaseConfig = {
+apiKey: "AIzaSyAIpdtOgSsv_PKJnA0kMk7fhqqD4yNaeZI",
+  authDomain: "sayitcutie.firebaseapp.com",
+  projectId: "sayitcutie",
+  storageBucket: "sayitcutie.firebasestorage.app",
+  messagingSenderId: "559001612992",
+  appId: "1:559001612992:web:593f8c9f41d1e9bfbe7325"
+};
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
