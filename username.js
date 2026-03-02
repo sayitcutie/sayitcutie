c9d0641bac04eddf3f473fd098915a5cf6395399
Cutiee <falturoaster18@gmail.com>
2026-03-03 02:01:01 +0530

    Create username.js

---
+++ username.js
@@ -0,0 +1,28 @@
+import {
+auth,
+db,
+doc,
+setDoc
+} from "./firebase.js";
+
+window.saveUsername = async () => {
+
+const username = document.getElementById("username").value.toLowerCase();
+
+const user = auth.currentUser;
+
+if(!user){
+alert("Login first");
+return;
+}
+
+await setDoc(doc(db,"users",user.uid),{
+
+username: username,
+email: user.email
+
+});
+
+window.location.href="dashboard.html";
+
+};
